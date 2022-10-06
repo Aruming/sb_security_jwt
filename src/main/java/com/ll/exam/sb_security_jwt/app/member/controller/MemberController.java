492060be7af8ce6da0a24f8438445362c4806ca2
Aruming <0430kar@naver.com>
2022-10-06 15:40:01 +0900

    MemberContext 추가

--- src/main/java/com/ll/exam/sb_security_jwt/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/sb_security_jwt/app/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ll.exam.sb_security_jwt.app.member.controller;
 import com.ll.exam.sb_security_jwt.app.base.dto.RsData;
 import com.ll.exam.sb_security_jwt.app.member.entity.Member;
 import com.ll.exam.sb_security_jwt.app.member.service.MemberService;
+import com.ll.exam.sb_security_jwt.app.security.entity.MemberContext;
 import com.ll.exam.sb_security_jwt.util.Util;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -10,11 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.net.http.HttpResponse;
@@ -26,6 +25,11 @@ import java.net.http.HttpResponse;
 public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/test")
+    public String test(@AuthenticationPrincipal MemberContext memberContext) {
+        return "안녕" + memberContext;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<RsData> login(@RequestBody LoginDto loginDto) {
