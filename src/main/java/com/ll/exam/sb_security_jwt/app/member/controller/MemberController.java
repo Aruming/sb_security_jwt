4e9752a556783bd9dc4441a122977648f5f6f5c4
Aruming <0430kar@naver.com>
2022-10-06 14:09:17 +0900

    작업

--- src/main/java/com/ll/exam/sb_security_jwt/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/sb_security_jwt/app/member/controller/MemberController.java
@@ -1,26 +1,45 @@
 package com.ll.exam.sb_security_jwt.app.member.controller;
 
+import com.ll.exam.sb_security_jwt.app.member.entity.Member;
+import com.ll.exam.sb_security_jwt.app.member.service.MemberService;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     @PostMapping("/login")
-    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
-        response.addHeader("Authentication", "JWT토큰");
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
 
-        return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "JWT키");
+
+        String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 
     @Data
     public static class LoginDto {
         private String username;
         private String password;
+
+        public boolean isNotValid() {
+            return username == null || password == null || username.trim().length() == 0 || password.trim().length() == 0;
+        }
     }
 }
