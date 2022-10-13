c3529be93e64e838fab6eefd1d61a1c09b50aa7f
Aruming <0430kar@naver.com>
2022-10-13 14:29:18 +0900

    캐시의 레퍼런스 키

---
+++ src/main/java/com/ll/exam/sb_security_jwt/app/cacheTest/dto/Person.java
@@ -0,0 +1,11 @@
+package com.ll.exam.sb_security_jwt.app.cacheTest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Person {
+    private long id;
+    private String name;
+}
--- src/main/java/com/ll/exam/sb_security_jwt/app/cacheTest/service/CacheTestService.java
+++ src/main/java/com/ll/exam/sb_security_jwt/app/cacheTest/service/CacheTestService.java
@@ -1,5 +1,6 @@
 package com.ll.exam.sb_security_jwt.app.cacheTest.service;
 
+import com.ll.exam.sb_security_jwt.app.cacheTest.dto.Person;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -9,13 +10,13 @@ import org.springframework.stereotype.Service;
 public class CacheTestService {
     @Cacheable("key1")
     public int getCachedInt(){
-        System.out.println("getCachedInt 호출됨");
+        System.out.println("== getCachedInt 실행 ==");
         return 5;
     }
 
     @CacheEvict("key1")
     public void deleteCacheKey1() {
-        System.out.println("삭제됨");
+        System.out.println("== deleteCacheKey1 실행 ==");
     }
 
     @CachePut("key1")
@@ -26,7 +27,13 @@ public class CacheTestService {
 
     @Cacheable("plus")
     public int getCachedPlus(int num1, int num2){
-        System.out.println("getCachedPlus 호출됨");
+        System.out.println("== getCachedPlus 실행 ==");
         return num1+num2;
+    }
+
+    @Cacheable(value = "getName", key = "#person.id")
+    public String getName(Person person, int random){
+        System.out.println("== getName 실행 ==");
+        return person.getName();
     }
 }
