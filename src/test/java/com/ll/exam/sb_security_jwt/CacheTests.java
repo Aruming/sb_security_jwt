a01f9ea5191aaddf805a8e5d013e3fbb472d6bef
Aruming <0430kar@naver.com>
2022-10-13 13:29:56 +0900

    캐시 삭제

--- src/test/java/com/ll/exam/sb_security_jwt/CacheTests.java
+++ src/test/java/com/ll/exam/sb_security_jwt/CacheTests.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 
 @SpringBootTest
 @Transactional
@@ -22,13 +20,24 @@ class CacheTests {
     @DisplayName("캐시 사용")
     void t1() throws Exception {
         int rs = memberService.getCachedInt();
-
-        assertThat(rs).isGreaterThan(0);
         System.out.println(rs);
 
         rs = memberService.getCachedInt();
+        System.out.println(rs);
+    }
 
-        assertThat(rs).isGreaterThan(0);
+    @Test
+    @DisplayName("캐시 삭제")
+    void t2() throws Exception {
+        int rs = memberService.getCachedInt();
+        System.out.println(rs);
+
+        rs = memberService.getCachedInt();
+        System.out.println(rs);
+
+        memberService.deleteCacheKey1();
+
+        rs = memberService.getCachedInt();
         System.out.println(rs);
     }
 }
